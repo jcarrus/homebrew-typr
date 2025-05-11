d359e3c0145b2b39f9fb162e10d00dedb44e9e8e
Justin Carrus <carrus.justin@gmail.com>
2025-05-11 12:03:11 -0700

    Update typr to v0.1.4

--- Formula/typr.rb
+++ Formula/typr.rb
@@ -1,9 +1,9 @@
 class Typr < Formula
   desc "Useful dictation app built with Tauri"
   homepage "https://github.com/jcarrus/typr"
-  url "https://github.com/jcarrus/typr/releases/download/v0.1.3/typr.tar.gz"
-  sha256 "63e178e8bf5220a89e95ebf3e2bbafea82cdeab24065adf4d956cae27763a8ee"
-  version "0.1.3"
+  url "https://github.com/jcarrus/typr/releases/download/v0.1.4/typr.tar.gz"
+  sha256 "0ac19018cf91e90c2f97c8d8bbbf856e0d3e93dd86e731d65b9c3d1ca2403afe"
+  version "0.1.4"
   
   def install
     prefix.install "typr.app"
